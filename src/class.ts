14205be1cb5b2fad717e865e52469570832edbe2
Fuad2199 <fuad.beybutov099@gmail.com>
2025-06-29 22:14:40 +0400

    feat(oop): add practice example for class

--- src/class.ts
+++ src/class.ts
@@ -1,3 +1,4 @@
+// Example class
 class Person {
     name: string;
     age: number;
@@ -15,3 +16,34 @@ class Person {
 // Creating an object
 const person1 = new Person("Alice", 25);
 person1.greet();
+
+// ================================================================================================
+
+class student {
+    fullName: string;
+    age: number;
+    studentId: string;
+    grades: number[];
+
+
+    constructor(fullName: string, age: number, studentId: string, grades: number[]) {
+        this.fullName = fullName;
+        this.age = age;
+        this.studentId = studentId;
+        this.grades = grades;
+    }
+
+    getAverageGrade(): number {
+        const total = this.grades.reduce((sum, grade) => sum + grade, 0);
+        return total / this.grades.length;
+    }
+
+    getStudentInfo(): string {
+        return `Student ${this.fullName} (${this.studentId}) is ${this.age} years old and have an average grade of `;
+    }
+
+}
+
+const student1 = new student("John Doe", 20, "S101", [90, 85, 80])
+
+console.log(student1.getStudentInfo() + student1.getAverageGrade() + ".")
